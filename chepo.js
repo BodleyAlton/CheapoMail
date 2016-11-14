37f051226972c639e80a0b4aa12b99625e941c0f
Alton <altonbodley@gmail.com>
2016-11-14 00:29:52 +0000

    v1.0.1

--- chepo.js
+++ chepo.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
     $('#login').on('click',login);
-    $('#newMail').on('click',compose)
+    $('#newMail').on('click',compose);
+    $('#Submit').on('click', createUser);
 });
 
 function compose(e){
-    $('#compose').toggle();
+    $('#compose').show();
+    $('#newMail').prop('disabled', true);
 }
 
 function login(e){
    e.preventDefault();
    console.log('Logged In');
+}
+function createUser(e){
+     e.preventDefault();
+     console.log('User created');
 }
