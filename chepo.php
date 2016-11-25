cdadbf090e64971714b4a235d09518574107867b
Alton <altonbodley@gmail.com>
2016-11-25 14:06:34 +0000

    Updated PHP POST variables and connected to DB

--- chepo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$post = $_POST['m'];
-$host= getenv('IP');
-$dbName=('schema');
-$dbUsername = getenv('C9 USER');
-$dbPassword = '';
-
-$conn = new PDO("mysql:host=$host;dbName=$dbname",$dbUsername,$dbPassword);
