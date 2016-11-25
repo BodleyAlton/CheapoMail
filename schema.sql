0076d4ca6df7c5082155e22682eb59ed3f41004c
Alton <altonbodley@gmail.com>
2016-11-25 14:08:21 +0000

    updated SQL syntax errors

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ CREATE DATABASE chepoMail;
 USE chepoMail;
 
 --Table structure for table users--
+
 DROP TABLE IF EXIST `users`;
 CREATE TABLE `users`(
     `id` int(5) NOT NULL auto_increment,
@@ -12,27 +13,36 @@ CREATE TABLE `users`(
     `password` char(30) NOT NULL default '',
     PRIMARY KEY (`username`),
     KEY (`firstname`),
-    KEY (`lastname`)
+    KEY (`lastname`),
+    KEY(`id`)
     );
     
---Table structure for table messagel--
+--Table structure for table message--
+
 DROP TABLE IF EXIST `message`;
 CREATE TABLE `message`(
     `id` int(50) NOT NULL auto_increment,
-    `recipient_ids` int(5) NOT NULL default '0',
-    `user_id` int(5) NOT NULL default '0',
+    `recipient_ids` int(5) NOT NULL default 00,
+    `user_id` int(5) NOT NULL default 00,
     `subject` text default '',
     `body` text default '',
-    `date_sent` date NOT NULL default GETDATE(),
+    `date_sent` date NOT NULL,
     PRIMARY KEY (`id`)
     );
 
 --Table structure for message_read--
+
 DROP TABLE IF EXIST `message_read`;
-CREATE TABLE `message_read(
+CREATE TABLE `message_read`(
     `id` int(50) NOT NULL auto_increment,
-    `message_id` int(50) NOT NULL default '',
-    `reader_id` int (5) NOT NULL default '',
-    `date_read` date NOT NULL default GETDATE(),
+    `message_id` int(50) NOT NULL default 00,
+    `reader_id` int (5) NOT NULL default 00,
+    `date_read` date NOT NULL,
     PRIMARY KEY (`id`)
     );
+
+INSERT INTO `users` VALUES(NULL,'Data','Admin','Administrator','Admin123');
+
+
+
+
